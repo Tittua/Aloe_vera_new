75e53890b0a52d659bb457192fa239546250599e
Tittu A <106869429+Tittua@users.noreply.github.com>
2023-08-21 17:22:27 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,32 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8bbe867b3bc1fb6e225828bf79620b9550181742282161950bc2d9693287230d
-size 809
+import streamlit as st
+from PIL import Image
+import ultralytics
+from ultralytics import YOLO
+
+st.title('Aloe Vera Plant Disease Analyser')
+
+col1,col2=st.columns(2)
+upload_stat=0
+with col1:
+    
+    input_image=st.file_uploader('Upload The Image File Here',type=['jpg','png'])
+    
+    confirmation=st.button('Submit')
+    if confirmation==True:
+        st.image(input_image)
+        upload_stat=1
+        
+with col2:
+    st.subheader('Detection:')
+    if upload_stat==1:
+        image_new=Image.open(input_image)
+        model=YOLO('best_new_v2.pt')
+        results= model(image_new)
+
+        for r in results:
+            im_array = r.plot()  # plot a BGR numpy array of predictions
+            im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+            st.image(im)  # show image
+
+    
+        
